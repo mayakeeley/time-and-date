3e81d86b390a986c9a2127160ccb8e72e54001a5
Adam Kovacs <adam.kovacs@Nology.local>
2019-11-12 10:08:22 +0000

    today Date test runs

--- main.js
+++ main.js
@@ -4,3 +4,7 @@ let lastName = "Kovacs";
 export const greet = () => {
   return `Hello, My name is ${firstName} ${lastName}`;
 };
+
+export const todaysDate = () => {
+  return Date();
+};
--- main.test.js
+++ main.test.js
@@ -1,6 +1,17 @@
 var solution = require("./main");
 import { greet } from "./main.js";
+import { todaysDate } from "./main.js";
 
 test("outputs fullname from ", () => {
   expect(solution.greet()).toBe("Hello, My name is Adam Kovacs");
+});
+
+test("Mock Date", () => {
+  const mockedDate = new Date(2017, 10, 10);
+  const originalDate = Date;
+
+  global.Date = jest.fn(() => mockedDate);
+  global.Date.setDate = originalDate.setDate;
+
+  expect(todaysDate().toString()).toBe(`${mockedDate}`);
 });
