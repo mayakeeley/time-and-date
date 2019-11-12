36ec5a3d7e81f0b9d3e33416c9def1fe0e996a28
Clara Fitzgerald <clara.fitzgerald@Nology.local>
2019-11-12 10:56:57 +0000

    finished

--- main.js
+++ main.js
@@ -1,12 +1,1 @@
-export const getTime = () => {
-  let d = new Date();
-  return d.getTime();
-};
 
-export const todaysDate = () => {
-  return Date();
-};
-
-export const secondsBetweenDates = (dateA, dateB) => {
-  return dateA * 1000 - dateB * 1000;
-};
