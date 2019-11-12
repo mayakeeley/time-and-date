310fdead51842acbd34904436bf73a6e5510a050
Maya Keeley <maya.keeley@Nology.local>
2019-11-12 10:12:18 +0000

    epoch

--- main.js
+++ main.js
@@ -4,3 +4,8 @@ let lastName = "Kovacs";
 export const greet = () => {
   return `Hello, My name is ${firstName} ${lastName}`;
 };
+
+export const getTime = () => {
+  let d = new Date();
+  return d.getTime();
+};
--- main.test.js
+++ main.test.js
@@ -1,6 +1,11 @@
 var solution = require("./main");
-import { greet } from "./main.js";
+import { greet, getTime } from "./main.js";
 
 test("outputs fullname from ", () => {
   expect(solution.greet()).toBe("Hello, My name is Adam Kovacs");
+});
+
+test("convert current date to milliseconds since epoch", () => {
+  let todaysDate = new Date();
+  expect(solution.getTime()).toBe(todaysDate.getTime());
 });
