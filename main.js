018d78e21b2d1aa4e37f26e81ba053c2a824bb40
Clara Fitzgerald <clara.fitzgerald@Nology.local>
2019-11-12 10:45:45 +0000

    seconds working

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-let firstName = "Adam";
-let lastName = "Kovacs";
+export const getTime = () => {
+  let d = new Date();
+  return d.getTime();
+};
 
-export const greet = () => {
-  return `Hello, My name is ${firstName} ${lastName}`;
+export const todaysDate = () => {
+  return Date();
+};
+
+export const secondsBetweenDates = (dateA, dateB) => {
+  return dateA * 1000 - dateB * 1000;
 };
--- main.test.js
+++ main.test.js
@@ -1,6 +1,24 @@
+import { getTime, todaysDate, secondsBetweenDates } from "./main.js";
 var solution = require("./main");
-import { greet } from "./main.js";
 
-test("outputs fullname from ", () => {
-  expect(solution.greet()).toBe("Hello, My name is Adam Kovacs");
+test("convert current date to milliseconds since epoch", () => {
+  let todaysDate = new Date();
+  expect(solution.getTime()).toBe(todaysDate.getTime());
+});
+
+test("Get todays date", () => {
+  const mockedDate = new Date(2017, 10, 10);
+  const originalDate = Date;
+
+  global.Date = jest.fn(() => mockedDate);
+  global.Date.setDate = originalDate.setDate;
+
+  expect(todaysDate().toString()).toBe(`${mockedDate}`);
+});
+
+test("Get seconds between todays date & another date", () => {
+  const todaysDate = new Date().getTime();
+  const givenDate = 1512864000000;
+
+  expect(secondsBetweenDates(todaysDate, givenDate)).toBe(-2592000000000);
 });
