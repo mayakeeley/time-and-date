34684af56bb0f36e359380488fb6d37b48809d5b
Dan Jones <dan.jones@Nology.local>
2019-11-12 10:22:41 +0000

    blah

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
-let firstName = "Adam";
-let lastName = "Kovacs";
+// let firstName = "Adam";
+// let lastName = "Kovacs";
 
-export const greet = () => {
-  return `Hello, My name is ${firstName} ${lastName}`;
+// export const greet = () => {
+//   return `Hello, My name is ${firstName} ${lastName}`;
+// };
+export const currentTime = new Date();
+// export const prevTime;
+const pastTime = () => {
+  const prevTime = currentTime.setDate(currentTime.getDate() - 1);
+  return console.log(prevTime);
+
+  // const hours = currentTime.getHours;
+  // const mins = currentTime.getMins;
+  // const secs = currentTime.getSeconds;
 };
